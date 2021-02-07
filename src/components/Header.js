c1d31200acce78c2738c2f29920ba71ee1f72911
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-07 22:25:49 +0300

    add className

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,13 @@ function Header({ links }) {
     const linkler = []
 
     links.forEach(({ id, title, link }) => {
-        linkler.push(<NavLink exact to={link} key={id}>{title}</NavLink>)
+        linkler.push(<NavLink exact className="navbar__link" to={link} key={id}>{title}</NavLink>)
     });
 
     return <>
         <header>
             <nav>
-                <div className="container">
+                <div className="navbar container">
                     {linkler}
                 </div>
             </nav>
