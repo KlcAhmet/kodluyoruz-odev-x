35463e42bd0c3223285bbe5a158548fe44e26aa6
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-07 21:41:20 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
+import React, { useState, useEffect } from "react"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
+import axios from "axios"
+
 /* import './App.css'; */
 
+
 function App() {
+
+  useEffect(() => {
+    /* linkteki data'yı getir */
+    axios.get('http://localhost:3004/headerLinks')
+      .then(function ({ data }) {
+        console.log({ data })
+        /* setLinks(data) */
+      })
+      .catch(function (error) {
+        console.log("Hata")
+        console.log(error)
+      })
+  }, [])
+
+
+
   return (
     <div className="App">
       test
