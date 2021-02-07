cac607d20c1dd48331e3009dac4c7dc207d94bde
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-07 23:19:06 +0300

    add

---
+++ src/components/Anasayfamain.js
@@ -0,0 +1,18 @@
+import Informationınput from "./Informationinput"
+
+function Anasayfamain() {
+    return (
+        <main>
+            <div className="container">
+                <section>
+                    <Informationınput />
+                </section>
+                <section>
+                    /////
+                </section>
+            </div>
+        </main>
+    )
+}
+
+export default Anasayfamain
