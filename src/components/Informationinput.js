d5447391e9de8597548b7a3b6297671db8462dad
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-08 01:32:15 +0300

    update

--- src/components/Informationinput.js
+++ src/components/Informationinput.js
@@ -1,8 +1,11 @@
+import Button from "./Button"
+
 function Informationınput() {
     return (
         <div className="inputs">
             <input id="isimInput" placeholder="isim" />
             <input id="bilgiInput" placeholder="Bilgi" />
+            <Button id={"inputbutton"} title={"Ekle"} className={"inputs__button btn btn-success"} />
         </div>
     )
 }
