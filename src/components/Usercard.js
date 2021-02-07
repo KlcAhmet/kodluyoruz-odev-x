651be1e784f49d37937f82cbd05ea4d06ae26d69
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-02-08 00:58:58 +0300

    extended function

--- src/components/Usercard.js
+++ src/components/Usercard.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
 
-function Usercard() {
-    const [user, setUser] = useState([])
+function Usercard({ name, desc }) {
 
+    const [user, setUser] = useState([])
     useEffect(() => {
         axios.get('http://localhost:3004/users')
             .then(function ({ data }) {
@@ -15,18 +15,36 @@ function Usercard() {
                 console.log(error)
             })
     }, [])
-
-    return user.map(({ name, desc }) => (
-        <div className="user-card">
-            <figure>
-                <img src="https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png" alt="user" width="40" />
-            </figure>
-            <div>
-                <strong>{name}</strong>
-                <p>{desc}</p>
+    if (name == null) {
+        return user.map(({ id, name, desc }) => (
+            <div key={id} className="user-card">
+                <figure>
+                    <img src="https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png" alt="user" width="40" />
+                </figure>
+                <div>
+                    <strong>{name}</strong>
+                    <p>{desc}</p>
+                </div>
             </div>
-        </div>
-    ));
+        ));
+    }
+    else {
+        return (
+            <div className="user-card">
+                <figure>
+                    <img src="https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png" alt="user" width="40" />
+                </figure>
+                <div>
+                    <strong>{name}</strong>
+                    <p>{desc}</p>
+                </div>
+            </div>
+        )
+    }
+
+
+
 }
+
 
 export default Usercard
